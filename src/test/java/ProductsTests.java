04170e2747e9dce5f278bbe0fc861e19a7007700
Your Name <you@example.com>
2023-12-20 00:18:18 +0300

    add provider

--- src/test/java/ProductsTests.java
+++ src/test/java/ProductsTests.java
@@ -3,12 +3,6 @@ import org.example.products.Ubuntu;
 import org.example.products.Windows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProductsTests {
