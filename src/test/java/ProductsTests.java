8c7c07d20014f212fd771c22344ae925f257d665
Your Name <you@example.com>
2023-12-20 00:17:35 +0300

    add provider

--- src/test/java/ProductsTests.java
+++ src/test/java/ProductsTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
