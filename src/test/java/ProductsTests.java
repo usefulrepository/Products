4ef5792331f1358d6db4eb5add11471224e4e2e5
Your Name <you@example.com>
2023-12-20 11:59:25 +0300

    add tasks

--- src/test/java/ProductsTests.java
+++ src/test/java/ProductsTests.java
@@ -7,6 +7,19 @@ import org.junit.jupiter.api.TestInstance;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProductsTests {
 
+    //todo: Дописать тест для проверки включения PostgreSQL
+    /*todo: Создать тесты для проверки включения нескольких PostgreSQL:
+           PostgreSQL.builder().osVersion("8").name("postgres-1").build();
+           PostgreSQL.builder().osVersion("7").name("postgres-2").build();
+           PostgreSQL.builder().osVersion("9").name("postgres-3").build();
+     */
+    /*todo: Создать тесты для проверки включения нескольких разных продуктов:
+       PostgreSQL.builder().osVersion("8").name("postgres-qwe").build();
+       Windows.builder().osVersion("11").name("windows-qwe").build();
+       Ubuntu.builder().osVersion("16").name("ubuntu-qwe").build();
+     */
+    //todo: Релизовать удаление продуктов по завершению тестов в классе
+
     @Test
     public void postgreSQLCreateTest(){
         var postgreSQL = PostgreSQL.builder()
